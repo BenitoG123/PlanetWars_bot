f9e58550705869231b83351b2fde5851ab21e223
BenitoG123 <bgravert@hotmail.com>
2025-07-10 15:09:32 -0700

    added actions

--- behavior_tree_bot/behaviors.py
+++ behavior_tree_bot/behaviors.py
@@ -1,6 +1,31 @@
 import sys
 sys.path.insert(0, '../')
 from planet_wars import issue_order
+import math
+
+def all_attack_weakest_enemy_planet(state): 
+    '''this attacks one enemy planet with every planet'''
+
+    # (2) Find total ships
+    total_planet_ships = sum(planet.num_ships for planet in state.my_planets())
+
+    # (3) Find the weakest enemy planet.
+    weakest_planet = min(state.enemy_planets(), key=lambda t: t.num_ships, default=None)
+
+    #calculate what percent of ships each planet needs to send and add 3 percent
+    required_percent = weakest_planet/total_planet_ships + 3
+
+    if not weakest_planet:
+        # No legal destination
+        return False
+    else:
+        try: #currently attacks one at a time, maybe switch having multiple planets attack
+            for planet in state.my_planets():
+                issue_order(state, planet.ID, weakest_planet.ID, planet.num_ships * required_percent)
+
+        except StopIteration:
+            # (4) Send required percent of ships from all planets to the weakest enemy planet.
+            return 
 
 
 def attack_weakest_enemy_planet(state): 
@@ -43,3 +68,94 @@ def spread_to_weakest_neutral_planet(state):
     else:
         # (4) Send half the ships from my strongest planet to the weakest enemy planet.
         return issue_order(state, strongest_planet.ID, weakest_planet.ID, strongest_planet.num_ships / 2)
+
+def spread_to_neutral_with_all(state):
+    my_planets = iter(sorted(state.my_planets(), key=lambda p: p.num_ships))
+    median_planet = my_planets[math.floor(len(my_planet)/2)]
+
+    neutral_planets = [planet for planet in state.neutral_planets()
+                      if not any(fleet.destination_planet == planet.ID for fleet in state.my_fleets())]
+    neutral_planets.sort(key=lambda p: p.num_ships) #neutral planets that I am not alraedy attacking
+
+    target_planets = iter(neutral_planets)
+
+    try: #currently attacks one at a time, maybe switch having multiple planets attack
+        my_planet = next(my_planets)
+        target_planet = next(target_planets)
+        sent_ships = 0
+        while True:
+            required_ships = target_planet.num_ships + 1
+            
+            #send 30% of ships from planet to target
+            ships = math.floor(my_planet.num_ships*0.3)
+            issue_order(state, my_planet.ID, target_planet.ID, ships)
+            my_planet = next(my_planets)
+            sent_ships += ships
+            if sent_ships > required_ships:
+                sent_ships = 0 #reset sent ships
+                target_planet = next(target_planets)
+
+    except StopIteration:
+        return 
+
+def spread_to_neutral_with_half(state):
+    my_planets = iter(sorted(state.my_planets(), key=lambda p: p.num_ships))
+    median_planet = my_planets[math.floor(len(my_planet)/2)]
+
+    neutral_planets = [planet for planet in state.neutral_planets()
+                      if not any(fleet.destination_planet == planet.ID for fleet in state.my_fleets())]
+    neutral_planets.sort(key=lambda p: p.num_ships) #neutral planets that I am not alraedy attacking
+
+    target_planets = iter(neutral_planets)
+
+    try: #currently attacks one at a time, maybe switch having multiple planets attack
+        my_planet = next(my_planets)
+        target_planet = next(target_planets)
+        sent_ships = 0
+        while True:
+            required_ships = target_planet.num_ships + 1
+            
+            #If planet has above average ships, send 30% of ships
+            if my_planet.num_ships > median_planet.num_ships:
+                ships = math.floor(my_planet.num_ships*0.3)
+                issue_order(state, my_planet.ID, target_planet.ID, ships)
+                my_planet = next(my_planets)
+                sent_ships += ships
+                if sent_ships > required_ships:
+                    target_planet = next(target_planets)
+            else:
+                sent_ships = 0 #reset sent ships
+                my_planet = next(my_planets)
+        
+
+    except StopIteration: #once go through all planets once, 
+                          #so may not go to all neutral planets in one turn, 
+                          #but that might be fine
+        return
+    
+def spread(state):
+    my_planets = iter(sorted(state.my_planets(), key=lambda p: p.num_ships))
+    #median_planet = my_planets[math.floor(len(my_planet)/2)]
+
+    neutral_planets = [planet for planet in state.neutral_planets()
+                      if not any(fleet.destination_planet == planet.ID for fleet in state.my_fleets())]
+    neutral_planets.sort(key=lambda p: p.num_ships) #neutral planets that I am not alraedy attacking
+
+    target_planets = iter(neutral_planets)
+
+    try: #currently attacks one at a time, maybe switch having multiple planets attack
+        my_planet = next(my_planets)
+        target_planet = next(target_planets)
+        while True:
+            required_ships = target_planet.num_ships + 1
+
+            if my_planet.num_ships > required_ships:
+                issue_order(state, my_planet.ID, target_planet.ID, required_ships)
+                my_planet = next(my_planets)
+                target_planet = next(target_planets)
+            else:
+                my_planet = next(my_planets)
+        
+
+    except StopIteration:
+        return
